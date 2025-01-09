daea4e7269cca1c2a45ae24463efe1f92abf384a
Jesus jose martha laborin <polibos6366@gmail.com>
2025-01-09 11:01:38 -0700

    variables in php

--- index.php
+++ index.php
@@ -1,16 +1,17 @@
-<?php
+ <?php
 
-echo "I like pizza <br>";
-echo "Its really good";
+    // echo "I like pizza <br>";
+    // echo "Its really good";
 
-// this is a comment
-/*this 
+    // this is a comment
+    /*this 
 is 
 a multiline comment 
 */
 
-?>
+    ?>
 
+ <!-- 
 <!DOCTYPE html>
 <html lang="en">
 
@@ -25,4 +26,45 @@ a multiline comment
     <button>order a pizza</button>
 </body>
 
-</html>
+</html> -->
+
+ <?php
+
+    //// VARIABLES
+    // strings
+    $name = "Jesus";
+    $food = "pizza";
+    $email = "fake@gmail.com";
+
+    // numbers int
+    $age = 21;
+    $users = 2;
+    $quantity = 3;
+
+    // numbers floats
+    $gpa = 2.5;
+    $price = 4.99;
+    $tax_rate = 5.1;
+
+
+    // booleans 
+    $employed = true;
+    $online = false;
+    $for_sale = true;
+
+
+    echo "Hello {$name} <br>";
+    echo "You like {$food} <br>";
+    echo "your email is {$email} <br>";
+    echo "You are {$age} years old <br>";
+    echo "There are {$users} users online <br>";
+    echo "You would like to buy {$quantity} items <br> ";
+
+    echo "Your gpa is: {$gpa} <br>";
+    echo "Your pizza is \${$price} <br>";
+    echo "the sales tax rate is: {$tax_rate} <br>";
+
+    echo "Online status: {$online}<br>"
+
+
+    ?>
