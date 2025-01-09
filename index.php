9b91ce69de08ea476fa4465d2fddc3fe930980cf
Jesus jose martha laborin <polibos6366@gmail.com>
2025-01-09 15:40:59 -0700

    and   superglobal variables

--- index.php
+++ index.php
@@ -65,37 +65,69 @@ a multiline comment
 
  <?php
 
-    // Arithmetic operators 
-    // + - * / ** %
+    // // Arithmetic operators 
+    // // + - * / ** %
 
-    $x = 10;
-    $y = 2;
-    $z = null;
+    // $x = 10;
+    // $y = 2;
+    // $z = null;
 
-    $z = $x + $y;
-    // echo $z;
-
-
-
-    // Incremen/ Decrement operators 
-    //++,--
-
-    $counter = 0;
-    $counter++;
-    $counter += 2;
-    // echo $counter;
-
-    $counter--;
-    // echo $counter;
+    // $z = $x + $y;
+    // // echo $z;
 
 
-    // Operator precedence 
-    // ()
-    // **
-    // * / %
-    // + - 
 
-    $total = 1 + 2 - 3 * 4 / 5 ** 6;
-    echo $total;
+    // // Incremen/ Decrement operators 
+    // //++,--
+
+    // $counter = 0;
+    // $counter++;
+    // $counter += 2;
+    // // echo $counter;
+
+    // $counter--;
+    // // echo $counter;
+
+
+    // // Operator precedence 
+    // // ()
+    // // **
+    // // * / %
+    // // + - 
+
+    // $total = 1 + 2 - 3 * 4 / 5 ** 6;
+    // echo $total;
+
+    ?>
+
+ <!DOCTYPE html>
+ <html lang="en">
+
+ <head>
+     <meta charset="UTF-8">
+     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <title>Document</title>
+ </head>
+
+ <body>
+     <form action="index.php" method="post">
+         <label>Username: </label><br>
+         <input type="text" name="username"><br>
+         <label>Password: </label><br>
+         <input type="password" name="password"><br>
+         <input type="submit" value="Log in">
+     </form>
+
+ </html>
+
+ <?php
+
+    // $_GET and $_POST global special variables 
+
+    echo "{$_POST["username"]}<br>";
+    echo "{$_POST["password"]}<br>";
+
+
+
 
     ?>
