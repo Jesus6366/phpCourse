b5aae403408de19a739a80b5b16e6efcd061684e
Jesus jose martha laborin <polibos6366@gmail.com>
2025-01-10 08:20:42 -0700

    and   superglobal variables

--- index.php
+++ index.php
@@ -111,11 +111,11 @@ a multiline comment
 
  <body>
      <form action="index.php" method="post">
-         <label>Username: </label><br>
-         <input type="text" name="username"><br>
-         <label>Password: </label><br>
-         <input type="password" name="password"><br>
-         <input type="submit" value="Log in">
+
+         <label for="">quantity:</label> <br>
+         <input type="text" name="quantity">
+
+         <input type="submit" value="total">
      </form>
 
  </html>
@@ -124,10 +124,14 @@ a multiline comment
 
     // $_GET and $_POST global special variables 
 
-    echo "{$_POST["username"]}<br>";
-    echo "{$_POST["password"]}<br>";
 
+    $item = "pizza";
+    $price = 5.99;
+    $quantity = $_POST["quantity"];
+    $total = null;
 
-
+    $total = $quantity * $price;
+    echo "You have ordered {$quantity} x {$item}/s <br> ";
+    echo "Your total is: \${$total}";
 
     ?>
